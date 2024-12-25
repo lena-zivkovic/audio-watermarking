8f0f33ceec3d64eb8cf2b9a0ed67f310582da72c
Lena Zivkovic <tanja.zivkovic03@gmail.com>
2024-12-25 01:21:22 -0500

    Created a plan for writing the program for LSB watermarking

---
+++ watermarking.py
@@ -0,0 +1,31 @@
+#
+# author: Lena Zivkovic
+# date: December 25, 2024
+#
+# watermarking.py: Embeding a watermark into an audio file using the LSB method
+#
+
+#
+# Importing an audio file
+# TODO
+#
+
+#
+# LSB algo for adding a watermark
+# TODO
+#
+
+#
+# Exporting the watermarked file
+# TODO
+#
+
+#
+# code for extracting the watermark
+# TODO
+#
+
+#
+# print the extracted watermark
+# TODO
+#
